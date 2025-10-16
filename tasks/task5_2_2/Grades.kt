18c694b7be509150ea7dd43373a9ed1f79794c97
koutherjafari <koutheraljafari@hotmail.com>
2025-10-16 15:28:58 +0000

    end of week 3 session 1

--- tasks/task5_2_2/Grades.kt
+++ tasks/task5_2_2/Grades.kt
@@ -1,1 +1,20 @@
 // Task 5.2.2: conversion of marks into grades, using a function
+fun grade(mark: Int) = when (mark) {
+    in 0..39   -> "Fail"
+    in 40..69  -> "Pass"
+    in 70..100 -> "Distinction"
+    else       -> "?"
+}
+
+fun main(args: Array<String>) {
+    if (args.size != 0) {
+        println("detected arguments")
+        for (mark in args) {
+            var result = grade(mark.toInt())
+            println("$mark is a $result")
+        }
+    }
+    else {
+        println("invalid arguments")
+    }
+}
